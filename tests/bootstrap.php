81e6ada122778de437273c209efc5132552e979e
Alexander Diebler <alexander.diebler@barzahlen.de>
2014-09-26 10:35:40 +0200

    fixed phpunit bootstrap for PHP 5.2

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,7 +8,7 @@ define('NOTIFICATIONKEY', 'e5354004de1001f86004090d01982a6e05da1c12');
 
 function emptyLog()
 {
-    fclose(fopen(__DIR__ . "/barzahlen.log", "w"));
+    fclose(fopen(dirname(__FILE__) . "/barzahlen.log", "w"));
 }
 
 function writeLog($logFile, $message)
